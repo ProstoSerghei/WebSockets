e6cddaa01edfad5c94c716b85caa076137941169
Serghei <prosto.serghei@yandex.ru>
2023-06-10 22:34:28 +0300

    Done

--- Lesson_1/task_6/task_6.py
+++ Lesson_1/task_6/task_6.py
@@ -1,33 +1,16 @@
 '''
 6. Создать текстовый файл test_file.txt, заполнить его тремя строками: «сетевое программирование», «сокет», «декоратор». Проверить кодировку файла по умолчанию. Принудительно открыть файл в формате Unicode и вывести его содержимое.
 '''
-import os
 
 
-DATA = [
-    'сетевое программирование',
-    'сокет',
-    'декоратр'
-]
 
-os.system('chardetect test_file.txt')
-
-
-# with open('test_file.txt', 'w') as f:
-#     for line in DATA:
-#         f.write(f'{line}\n')
-
-
-with open('test_file.txt', 'r', encoding='utf-8') as f:
-    for line in f:
-        print(line, end='')
+with open('test_file.txt', 'rb') as f:
+    print(f.read().decode('utf-8'))
 
 
 
 '''
 Output:
-
-test_file.txt: utf-8 with confidence 0.99
 
 сетевое программирование
 сокет
